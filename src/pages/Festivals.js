283a1226435d0b662b7648ea5b58c774234187de
Emrullah Ayın <e.ayin@jollytur.com>
2019-04-22 11:40:20 +0300

    config

--- src/pages/Festivals.js
+++ src/pages/Festivals.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Festivals = () => {
-  return <h1>Festivals</h1>;
+  return (
+    <Fragment>
+      <h1>Festivals</h1>
+      <p>
+        Quisque ac tincidunt urna. Phasellus rutrum bibendum pulvinar. Fusce
+        convallis rhoncus ex, eu vulputate leo dapibus id. Nam neque nulla,
+        feugiat ut facilisis a, ullamcorper vel sapien. Etiam quis felis eget
+        augue euismod volutpat. Ut aliquet convallis posuere. Fusce bibendum
+        purus in nulla commodo scelerisque. Nunc dapibus porta viverra. Praesent
+        ut mauris vestibulum, finibus nunc in, eleifend risus. Nullam ac
+        ultricies metus. In non lobortis velit. Nullam ornare gravida fringilla.
+      </p>
+    </Fragment>
+  );
 };
 
 export default Festivals;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Home = () => {
   return (
-    <div>
+    <Fragment>
       <h1>Home</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet,
-        purus vitae eleifend tristique, lorem magna volutpat orci, et vehicula
-        erat erat nec elit. Aenean posuere nunc ac cursus facilisis. Aenean vel
-        porta turpis, ut iaculis justo.
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
       </p>
-    </div>
+    </Fragment>
   );
 };
 
